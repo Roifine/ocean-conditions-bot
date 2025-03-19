ddde946b15c333d88a3606e4d15af07f7ca4ca36
Roi Fine <fineroi@gmail.com>
2025-03-19 17:05:35 +1100

    accounted for beach facing degree in wind function

--- debug_read_and_print.py
+++ debug_read_and_print.py
@@ -6,13 +6,13 @@ import json # handles reading and writting json files
 import math
 
 # Function Handling time from UTC to Sydney time
-def timezone(xtime):
+def timezone(time):
 
     utc_zone = pytz.utc
     sydney_zone = pytz.timezone("Australia/Sydney")
 
-    xtime = xtime.replace(tzinfo=utc_zone)
-    sydney_time = xtime.astimezone(sydney_zone)
+    time = time.replace(tzinfo=utc_zone)
+    sydney_time = time.astimezone(sydney_zone)
 
     return sydney_time
 
@@ -26,13 +26,11 @@ def wind_strength(wind_speed_meters_ps: float):
         return "Moderate"
     elif 20 <= wind_speed_km_ph:
         return "Strong"
-    
-def compass_add(a, b):
-    result = (a + b) % 360
-    return result if result >= 0 else result + 360
+
+# function to convert degrees to name of swell & wind direction 
 
 
-def degrees(degree: float): # Currently not in use! function to convert degrees to name of swell & wind direction 
+def degrees(degree: float):
     if degree >= 330 or degree <= 30:
         return f"North ({degree}°)"
     elif 30 < degree <= 60:
@@ -59,13 +57,13 @@ def effective_wind_direction(wind_direction :float, beach_facing_degree: int):
     else:
         return f"cross-shore"
 
-def effective_wave_size(size_meter: float, direction: float, period: float, beach_facing_degree: float):
+def effective_wave_size(size_meter: float, direction: float, period: float):
     size_feet = size_meter / 0.305
-    if not beach_facing_degree + 45 > direction > beach_facing_degree - 45:
-        size_feet *= 0.7
-    if 11 > period > 8:
+    if not 110 < direction < 190:
+        size_feet *= 0.6
+    if 11 >= period > 8:
         size_feet *= 1.1
-    elif period >= 11:
+    elif period > 11:
         size_feet *= 1.3
     low = math.floor(size_feet)
     high = math.ceil(size_feet)
@@ -80,9 +78,8 @@ surf = json.loads(wave_data) # the whole json file content for wave hight
 wind = json.loads(wind_data) # the whole json file content for wind hight
 
 
+beach_facing_degree = 115 #hard coding to Bondi, this is used in the wind and wave size functions as an input
 
-
-beach_facing_degree = 145 #hard coding to Bondi, this is used in the wind and wave size functions as an input
 heights = surf['hours']
 wave_dic = {}
 for height in heights:
@@ -93,8 +90,8 @@ for height in heights:
 
     
     wave_dic[time] = {}
-    wave_dic[time]['size'] = effective_wave_size(size_meter, direction, period, beach_facing_degree) # for the size key in the dictioary add the size as a value in a list of values
-    wave_dic[time]['direction'] = degrees(direction)
+    wave_dic[time]['size'] = size_meter 
+    wave_dic[time]['direction'] = direction
     wave_dic[time]['period'] = period
 
        
@@ -106,8 +103,8 @@ for wind in winds:
     wind_speed_meters_ps = float(wind['windSpeed']['sg'])
     time = timezone(datetime.strptime(wind['time'], "%Y-%m-%dT%H:%M:%S%z")) # convert the time into datetime object
 
-    wave_dic[time]['wind_direction'] = effective_wind_direction(wind_direction, beach_facing_degree)
-    wave_dic[time]['wind_speed_km_ph'] = wind_strength(wind_speed_meters_ps) 
+    wave_dic[time]['wind_direction'] = wind_direction
+    wave_dic[time]['wind_speed_km_ph'] = wind_speed_meters_ps 
 
 
 # printing for 8 am next ten days
@@ -122,9 +119,12 @@ count = 0
 for time, values in wave_dic.items():
     if time.hour == 8:
         print(f"""
-        🗓️ {time.strftime("%A")}
-        🏄 {values['size']} ft
-        💨 {values['wind_speed_km_ph']} {values['wind_direction']} wind
+        🌊 {time.strftime("%A")}
+        wave size {values['size']}m
+        wave direction {(values['direction'])}°
+        wave period {(values['period'])}
+        wind speed {(values['wind_speed_km_ph'])} 
+        wind direction {(values['wind_direction'])}° 
        """)
         count += 1
         if count == 5:
