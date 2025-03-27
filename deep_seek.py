58bd77206951585d305c53130a35d2ad45eb40a3
Roi Fine <fineroi@gmail.com>
2025-03-27 15:02:50 +1100

    created a new program to engage with deepseek chat

---
+++ deep_seek.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+import os # to load the api keys from my env file
+from dotenv import load_dotenv # to load the api keys from my env file
+
+if os.getenv("GITHUB_ACTIONS") is None: # Load environment variables from .env only if running locally
+    load_dotenv("api_keys.env")
+
+deepseek_api = os.getenv("deepseek_api")  # Now, API_KEY contains "your_secret_key_here"
+
+client = OpenAI(api_key=deepseek_api, base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
